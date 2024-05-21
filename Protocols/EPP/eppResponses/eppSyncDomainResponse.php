9e995cbb2aed80245ff2b267e0c5842f303f2c29
dimitri-khelaia <dimitri.khelaia@portosoftware.com>
2024-05-21 21:13:25 +0400

    test

--- Protocols/EPP/eppResponses/eppSyncDomainResponse.php
+++ Protocols/EPP/eppResponses/eppSyncDomainResponse.php
@@ -4,11 +4,7 @@ namespace Metaregistrar\EPP;
 use SimpleXMLElement;
 
 class eppSyncDomainResponse extends eppResponse {
-    /**
-     * SimpleXMLElement representation of the response
-     * @var SimpleXMLElement
-     */
-    private $xml;
+
 
     /**
      * eppSyncDomainResponse constructor.
@@ -18,32 +14,5 @@ class eppSyncDomainResponse extends eppResponse {
      */
     public function __construct($response) {
         parent::__construct($response);
-        $this->xml = new SimpleXMLElement($response);
-    }
-
-    /**
-     * Check if the sync operation was successful
-     * @return bool
-     */
-    public function isSuccess() {
-        $resultCode = $this->xml->response->result['code'];
-        return in_array((string)$resultCode, ['1000', '1001']); // Common success codes in EPP
-    }
-
-    /**
-     * Retrieve the result code from the response
-     * @return string|null
-     */
-    public function getResultCode() {
-        return (string)$this->xml->response->result['code'];
-    }
-
-    /**
-     * Get a human-readable message from the response
-     * @return string|null
-     */
-    public function getMessage() {
-        return (string)$this->xml->response->result->msg;
     }
 }
-?>
