61a58d0c5a19325a30c689ad70e32aaaa0e66a65
dimitri-khelaia <dimitri.khelaia@portosoftware.com>
2024-05-23 21:47:09 +0400

    add sync command of verisign

--- Protocols/EPP/eppRequests/eppSyncDomainRequest.php
+++ Protocols/EPP/eppRequests/eppSyncDomainRequest.php
@@ -5,51 +5,58 @@ namespace Metaregistrar\EPP;
 use DateTime;
 use SimpleXMLElement;
 
-class eppSyncDomainRequest extends eppDomainRequest {
-    /**
-     * Domain name to be synced
-     * @var string
-     */
+class eppSyncDomainRequest extends eppRequest {
+
     private $domainName;
+    private $expMonthDay;
 
-    /**
-     * Sync date formatted as YYYY-MM-DD
-     * @var string
-     */
-    private $syncDateFormatted;
-
-    /**
-     * eppSyncDomainRequest constructor.
-     * @param string $domainName Domain to be synced
-     * @param DateTime|null $syncDate Date to sync the domain to, defaults to 15th of the next month
-     */
-    public function __construct($domainName, $syncDate = null) {
-        parent::__construct('sync');
+    public function __construct($domainName, $expMonthDay) {
+        parent::__construct();
         $this->domainName = $domainName;
-        $this->syncDateFormatted = $syncDate ? $syncDate->format('Y-m-d') : (new DateTime('first day of next month'))->modify('+14 days')->format('Y-m-d');
+        $this->expMonthDay = $expMonthDay;
+        $this->build();
     }
 
-    /**
-     * Overriding a hypothetical buildCommand() method from the base class to construct the specific XML for sync command.
-     */
-    protected function buildCommand() {
+    public function build() {
+
         $update = $this->createElement('update');
+        $this->getCommand()->appendChild($update);
+
+        // <domain:update> element
         $domainUpdate = $this->createElement('domain:update');
         $domainUpdate->setAttribute('xmlns:domain', 'urn:ietf:params:xml:ns:domain-1.0');
-
-        $nameElement = $this->createElement('domain:name', $this->domainName);
-        $domainUpdate->appendChild($nameElement);
-
-        $extension = $this->createElement('extension');
-        $syncElement = $this->createElement('sync:update');
-        $syncElement->setAttribute('xmlns:sync', 'urn:ietf:params:xml:ns:epp-sync-1.0');
-        $syncDateElement = $this->createElement('sync:date', $this->syncDateFormatted);
-        $syncElement->appendChild($syncDateElement);
-
-        $extension->appendChild($syncElement);
+        $domainUpdate->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $domainUpdate->setAttribute('xsi:schemaLocation', 'urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd');
         $update->appendChild($domainUpdate);
-        $update->appendChild($extension);
 
-        $this->epp->appendChild($update);
+        // <domain:name> element
+        $domainName = $this->createElement('domain:name', $this->domainName);
+        $domainUpdate->appendChild($domainName);
+
+        // <domain:chg> element
+        $domainChg = $this->createElement('domain:chg');
+        $domainUpdate->appendChild($domainChg);
+
+        // <extension> element
+        $extension = $this->getExtension();
+
+        $tld = substr(strrchr($this->domainName, '.'), 1);
+        $namestoreExt = $this->createElement('namestoreExt:namestoreExt');
+        $namestoreExt->setAttribute('xmlns:namestoreExt', 'http://www.verisign-grs.com/epp/namestoreExt-1.1');
+        $namestoreExt->appendChild($this->createElement('namestoreExt:subProduct', strtoupper($tld)));
+        $extension->appendChild($namestoreExt);
+
+        // <sync:update> element
+        $syncUpdate = $this->createElement('sync:update');
+        $syncUpdate->setAttribute('xmlns:sync', 'http://www.verisign.com/epp/sync-1.0');
+        $syncUpdate->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $syncUpdate->setAttribute('xsi:schemaLocation', 'http://www.verisign.com/epp/sync-1.0 sync-1.0.xsd');
+        $extension->appendChild($syncUpdate);
+
+        // <sync:expMonthDay> element
+        $expMonthDay = $this->createElement('sync:expMonthDay', $this->expMonthDay);
+        $syncUpdate->appendChild($expMonthDay);
+
     }
+
 }
