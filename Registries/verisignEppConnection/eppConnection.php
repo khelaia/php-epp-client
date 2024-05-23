a216f8780b3ec584b09dcc361d31e174c3a7adc9
dimitri-khelaia <dimitri.khelaia@portosoftware.com>
2024-05-23 19:46:44 +0400

    add sync method to verisign epp connection

--- Registries/verisignEppConnection/eppConnection.php
+++ Registries/verisignEppConnection/eppConnection.php
@@ -15,4 +15,40 @@ class verisignEppConnection extends eppConnection {
         parent::enableLaunchphase('claims');
     }
 
+    public function syncDomain($domainName, $newExpirationDate) {
+        $xml = $this->createSyncCommandXML($domainName, $newExpirationDate);
+        return $this->writeAndRead($xml);
+    }
+
+    private function createSyncCommandXML($domainName, $newExpirationDate) {
+        $xml = new \DOMDocument('1.0', 'UTF-8');
+
+        $epp = $xml->createElement('epp');
+        $epp->setAttribute('xmlns', 'urn:ietf:params:xml:ns:epp-1.0');
+        $epp->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+        $epp->setAttribute('xsi:schemaLocation', 'urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd');
+        $xml->appendChild($epp);
+
+        $command = $xml->createElement('command');
+        $epp->appendChild($command);
+
+        $sync = $xml->createElement('sync');
+        $sync->setAttribute('xmlns', 'urn:ietf:params:xml:ns:sync-1.0');
+        $command->appendChild($sync);
+
+        $domain = $xml->createElement('domain');
+        $sync->appendChild($domain);
+
+        $name = $xml->createElement('name', $domainName);
+        $domain->appendChild($name);
+
+        $exDate = $xml->createElement('exDate', $newExpirationDate);
+        $domain->appendChild($exDate);
+
+        $clTRID = $xml->createElement('clTRID', 'ABC-12345');
+        $command->appendChild($clTRID);
+
+        return $xml->saveXML();
+    }
+
 }
